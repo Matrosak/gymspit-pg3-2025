1af0581d6a56ff6894738a28717b2fdde3c276d5
Matrosak <117300140+Matrosak@users.noreply.github.com>
2025-10-13 13:36:55 +0200

    kalkulacka
    Dobrý den pane profesore, omlouvam se vcera jsem nemel pristup k pocitaci. děkuji moc za pochopení, Matyáš Nechvátal

---
+++ kalkulacka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej první číslo: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Zadej operaci (+, -, *, /): ");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.Write("Zadej druhé číslo: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double vysledek = 0;
+
+        if (op == '+') vysledek = a + b;
+        else if (op == '-') vysledek = a - b;
+        else if (op == '*') vysledek = a * b;
+        else if (op == '/') vysledek = a / b;
+        else Console.WriteLine("Neplatná operace!");
+
+        Console.WriteLine("Výsledek: " + vysledek);
+    }
+}
